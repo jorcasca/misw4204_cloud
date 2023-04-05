4944f3732110f049dca6a0c74fcd4d5899f1b7cc
jcastanov <j.castanov@uniandes.edu.co>
2023-04-05 00:00:50 -0500

    feat(Comprimir): comprimir en formatos y borrar

--- Microservicios_Cloud/microservicio_auth/app.py
+++ Microservicios_Cloud/microservicio_auth/app.py
@@ -1,6 +1,6 @@
 from microservicio_auth import create_app
 from flask_restful import Api
-from .vistas import VistaRuta, VistaAuthSignup, VistaLogin, VistaTasks
+from .vistas import VistaAuthSignup, VistaLogin, VistaTasks, VistaTask
 from .modelos import db
 from flask_jwt_extended import JWTManager
 
@@ -15,9 +15,9 @@ db.init_app(app)
 db.create_all()
 
 api = Api(app)
-api.add_resource(VistaRuta, '/rutas')
 api.add_resource(VistaAuthSignup, '/api/auth/signup')
 api.add_resource(VistaLogin, '/api/auth/login')
 api.add_resource(VistaTasks, '/api/tasks')
+api.add_resource(VistaTask, '/api/tasks/<int:id_task>')
 
 jwt = JWTManager(app)
--- Microservicios_Cloud/microservicio_auth/vistas/vistas.py
+++ Microservicios_Cloud/microservicio_auth/vistas/vistas.py
@@ -1,40 +1,41 @@
 from flask_restful import Resource
-from ..modelos import Ruta, RutaSchema, Usuario, db, Tareas, TareasSchema
-from flask_jwt_extended import jwt_required, create_access_token
+from ..modelos import Usuario, db, Tareas, TareasSchema
+from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity
 from flask import request
+import os
+import zipfile
+import py7zr
+import tarfile
 
-ruta_schema = RutaSchema()
 tareas_schema = TareasSchema()
 
-
-class VistaRuta(Resource):
-    def get(self):
-        return [ruta_schema.dump(ruta) for ruta in Ruta.query.all()]
-        
 class VistaAuthSignup(Resource):
+    
     def post(self):
-        usuario = Usuario.query.filter(Usuario.username == request.json["username"]).first()
-        if  (request.json["password1"] != request.json["password2"]):
+        usuario = Usuario.query.filter(
+            Usuario.username == request.json["username"]).first()
+        if (request.json["password1"] != request.json["password2"]):
             return "El password ingresado No coincide", 404
         elif usuario is not None:
             return "El usuario {} ya existe".format(request.json["username"]), 404
         else:
-            email = Usuario.query.filter(Usuario.email == request.json["email"]).first()
+            email = Usuario.query.filter(
+                Usuario.email == request.json["email"]).first()
             if email is not None:
                 return "El correo ingresado {} ya existe".format(request.json["email"]), 404
             else:
-                nuevo_usuario = Usuario(username = request.json["username"],
-                                        password = request.json["password1"],
-                                        email = request.json["email"]
-                                        ) 
+                nuevo_usuario = Usuario(username=request.json["username"],
+                                        password=request.json["password1"],
+                                        email=request.json["email"]
+                                        )
                 db.session.add(nuevo_usuario)
                 db.session.commit()
                 return "Se creo el usuario satisfactoriamente."
-            
-            
+
+
 class VistaLogin(Resource):
+    
     def post(self):
-        #contrasena_encriptada = hashlib.md5(request.json["contrasena"].encode('utf-8')).hexdigest()
         usuario = Usuario.query.filter(Usuario.username == request.json["username"],
                                        Usuario.password == request.json["password"]).first()
         db.session.commit()
@@ -43,13 +44,12 @@ class VistaLogin(Resource):
         else:
             token_de_acceso = create_access_token(identity=usuario.id)
             return {"mensaje": "Inicio de sesión exitoso", "token": token_de_acceso, "id": usuario.id, "username": usuario.username}
-        
-        
+
+
 class VistaTasks(Resource):
-    
+
     @jwt_required()
     def get(self):
-        
         if (request.json["max"] is None):
             if (int(request.json["order"]) == 0):
                 tasks = Tareas.query.order_by(Tareas.id.desc()).all()
@@ -59,13 +59,67 @@ class VistaTasks(Resource):
                 tasks = Tareas.query.order_by(Tareas.id.asc()).all()
         else:
             if (int(request.json["max"]) > 0) & (int(request.json["order"]) == 0):
-                tasks = Tareas.query.order_by(Tareas.id.desc()).limit(int(request.json["max"]))
+                tasks = Tareas.query.order_by(
+                    Tareas.id.desc()).limit(int(request.json["max"]))
             elif (int(request.json["max"]) > 0) & (int(request.json["order"]) == 1):
                 tasks = Tareas.query.limit(int(request.json["max"]))
             else:
                 tasks = Tareas.query.order_by(Tareas.id.asc()).all()
-            
         return [tareas_schema.dump(tarea) for tarea in tasks]
 
+    @jwt_required()
+    def post(self):
+        file = request.files.get('fileName')
+        new_format = request.form.get('newFormat')
+        user_id = get_jwt_identity()
+        if not os.path.exists('archivos'):
+            os.makedirs('archivos/originales')
+        file_name = file.filename
+        file.save(os.path.join('archivos/originales', file_name))
+        nueva_tarea = Tareas(fileName = file_name, newFormat = new_format, status = 'uploaded', usuario = user_id)
+        db.session.add(nueva_tarea)
+        db.session.commit()
+        # Descomentar el siguiente return cuando la logica posterior se pase a colas
+        # return {'message': 'la tarea fue creada.'}
+    
+        # Todo el siguiente bloque debe ir en colas
+        if not os.path.exists('archivos/convertidos'):
+            os.makedirs('archivos/convertidos')     
+        if new_format == 'ZIP':
+            file_path = os.path.join('archivos/originales', file_name)
+            zip_file_path = os.path.join('archivos/convertidos', file_name + '.zip')
+            with zipfile.ZipFile(zip_file_path, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+                zip_file.write(file_path, file_name)
+        elif new_format == '7Z':
+            file_path = os.path.join('archivos/originales', file_name)
+            seven_zip_file_path = os.path.join('archivos/convertidos', file_name + '.7z')
+            with py7zr.SevenZipFile(seven_zip_file_path, 'w') as seven_zip_file:
+                seven_zip_file.write(file_path, file_name)
+        elif new_format == 'TAR.GZ':
+            file_path = os.path.join('archivos/originales', file_name)
+            tar_gz_file_path = os.path.join('archivos/convertidos', file_name + '.tar.gz')
+            with tarfile.open(tar_gz_file_path, 'w:gz') as tar_gz_file:
+                tar_gz_file.add(file_path, file_name)
+        else:
+            return {'message': 'Formato no soportado'}
+        nueva_tarea.status = 'processed'
+        db.session.commit()
+        return {'message': 'Archivo recibido y procesado correctamente.'}
 
-       
+
+class VistaTask(Resource):
+        
+    @jwt_required()
+    def delete(self, id_task):
+        tarea = Tareas.query.get_or_404(id_task)
+        if tarea.status != 'processed':
+            return {'message': 'La tarea no ha sido procesada aún'}, 400
+        archivo_original_path = os.path.join('archivos/originales', tarea.fileName)
+        archivo_convertido_path = os.path.join('archivos/convertidos', tarea.fileName + '.' + tarea.newFormat.lower())
+        if os.path.exists(archivo_original_path):
+            os.remove(archivo_original_path)
+        if os.path.exists(archivo_convertido_path):
+            os.remove(archivo_convertido_path)
+        db.session.delete(tarea)
+        db.session.commit()
+        return {'message': 'Tarea y archivos eliminados correctamente.'}
