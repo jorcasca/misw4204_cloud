be5d9e5394780f6615602c08057308c705c2ec6e
JamesMontealegre <js.montealegre@uniandes.edu.co>
2023-04-12 11:30:26 -0500

    refactor(*): change some endpoints to adapt acceptance criteria

--- Microservicios_Cloud/microservicio_auth/vistas/vistas.py
+++ Microservicios_Cloud/microservicio_auth/vistas/vistas.py
@@ -3,16 +3,37 @@ from ..modelos import Usuario, db, Tareas, TareasSchema
 from ..tareas import convert_file
 from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity
 from flask import request, send_file
+from sqlalchemy import or_
 import os
 
 tareas_schema = TareasSchema()
 
+
+def validateStrongPassword(password):
+    if len(password) < 8:
+        return False, "El password debe tener al menos 8 caracteres."
+
+    if not any(c.isupper() for c in contrasena):
+        return False, "La contraseña debe tener al menos una letra mayúscula."
+
+    if not any(c.isdigit() for c in contrasena):
+        return False, "La contraseña debe tener al menos un número."
+
+    return True
+
+
 class VistaAuthSignup(Resource):
-    
+
     def post(self):
         usuario = Usuario.query.filter(
             Usuario.username == request.json["username"]).first()
-        if (request.json["password1"] != request.json["password2"]):
+
+        validate_passwd = validateStrongPassword(request.json["password1"])
+
+        if not validate_passwd:
+            return "El password ingresado no cumple con los requesitos minimos de seguridad", 400
+
+        elif (request.json["password1"] != request.json["password2"]):
             return "El password ingresado No coincide", 404
         elif usuario is not None:
             return "El usuario {} ya existe".format(request.json["username"]), 404
@@ -32,10 +53,13 @@ class VistaAuthSignup(Resource):
 
 
 class VistaLogin(Resource):
-    
+
     def post(self):
-        usuario = Usuario.query.filter(Usuario.username == request.json["username"],
-                                       Usuario.password == request.json["password"]).first()
+        usuario = Usuario.query.filter(
+            or_(Usuario.username == request.json.get("username"),
+                Usuario.email == request.json.get("username")),
+            Usuario.password == request.json.get("password")
+        ).first()
         db.session.commit()
         if usuario is None:
             return "Usuario o password Incorrectos", 404
@@ -48,21 +72,23 @@ class VistaTasks(Resource):
 
     @jwt_required()
     def get(self):
-        if (request.json["max"] is None):
+        if (request.json["max"] is not None and request.json["order"] is not None):
             if (int(request.json["order"]) == 0):
                 tasks = Tareas.query.order_by(Tareas.id.desc()).all()
+
             elif (int(request.json["order"]) == 1):
                 tasks = Tareas.query.order_by(Tareas.id.asc()).all()
-            else:
-                tasks = Tareas.query.order_by(Tareas.id.asc()).all()
-        else:
-            if (int(request.json["max"]) > 0) & (int(request.json["order"]) == 0):
+
+            elif (int(request.json["max"]) > 0) & (int(request.json["order"]) == 0):
                 tasks = Tareas.query.order_by(
                     Tareas.id.desc()).limit(int(request.json["max"]))
+
             elif (int(request.json["max"]) > 0) & (int(request.json["order"]) == 1):
                 tasks = Tareas.query.limit(int(request.json["max"]))
-            else:
-                tasks = Tareas.query.order_by(Tareas.id.asc()).all()
+
+        else:
+            tasks = Tareas.query.order_by(Tareas.id.asc()).all()
+
         return [tareas_schema.dump(tarea) for tarea in tasks]
 
     @jwt_required()
@@ -74,7 +100,8 @@ class VistaTasks(Resource):
             os.makedirs('archivos/originales')
         file_name = file.filename
         file.save(os.path.join('archivos/originales', file_name))
-        nueva_tarea = Tareas(fileName=file_name, newFormat=new_format, status='uploaded', usuario=user_id)
+        nueva_tarea = Tareas(
+            fileName=file_name, newFormat=new_format, status='uploaded', usuario=user_id)
         db.session.add(nueva_tarea)
         db.session.commit()
         convert_file.delay(nueva_tarea.id)
@@ -82,14 +109,16 @@ class VistaTasks(Resource):
 
 
 class VistaTask(Resource):
-        
+
     @jwt_required()
     def delete(self, id_task):
         tarea = Tareas.query.get_or_404(id_task)
         if tarea.status != 'processed':
             return {'message': 'La tarea no ha sido procesada aún'}, 400
-        archivo_original_path = os.path.join('archivos/originales', tarea.fileName)
-        archivo_convertido_path = os.path.join('archivos/convertidos', tarea.fileName + '.' + tarea.newFormat.lower())
+        archivo_original_path = os.path.join(
+            'archivos/originales', tarea.fileName)
+        archivo_convertido_path = os.path.join(
+            'archivos/convertidos', tarea.fileName + '.' + tarea.newFormat.lower())
         if os.path.exists(archivo_original_path):
             os.remove(archivo_original_path)
         if os.path.exists(archivo_convertido_path):
@@ -97,18 +126,22 @@ class VistaTask(Resource):
         db.session.delete(tarea)
         db.session.commit()
         return {'message': 'Tarea y archivos eliminados correctamente.'}
-    
 
     @jwt_required()
     def get(self, id_task):
         tarea = Tareas.query.get_or_404(id_task)
-        return  tareas_schema.dump(tarea)
-    
+        return tareas_schema.dump(tarea)
+
+
+class VistaFile(Resource):
+
     @jwt_required()
-    def post(self, id_task):
-        tarea = Tareas.query.get_or_404(id_task)
-        if (int(request.json["status"]) == 0):
-            return send_file('archivos/originales/'+tarea.fileName, as_attachment=True)
-        elif (int(request.json["status"]) == 1 and tarea.status=='processed'):
-            return send_file('archivos/convertidos/'+tarea.fileName+'.'+ (tarea.newFormat).lower(), as_attachment=True)
-        return {'message':'Se debe seleccionar la tareas a realizar 0 para descargar el archivo original, 1 para descargar el archivo convertido, si eligión el convertido, y no descargó es porque no ha procesado'}
+    def get(self, filename):
+        try:
+            nombre, extension = os.path.splitext(filename)
+            if extension:
+                return send_file('archivos/convertidos/'+filename, as_attachment=True)
+            else:
+                return send_file('archivos/originales/'+filename, as_attachment=True)
+        except FileNotFoundError:
+            abort(404)
