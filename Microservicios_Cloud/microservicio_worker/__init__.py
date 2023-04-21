5b2a36bf54dfd43f01c7dc2a4dfa22c2c73147e1
rcelisc <r.celis@uniandes.edu.co>
2023-04-20 19:38:53 -0500

    Update __init__.py
    Actualizacion conexion nube.

--- Microservicios_Cloud/microservicio_worker/__init__.py
+++ Microservicios_Cloud/microservicio_worker/__init__.py
@@ -3,7 +3,7 @@ from .modelos import db
 
 def create_app(config_name):
     app = Flask(__name__)  
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:contrasena123@localhost/baseapp'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:contrasena123@34.171.28.53/baseapp'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
     app_context = app.app_context()
     app_context.push()
